b9637cfb95b04610d7758cc471b3af9db384974b
dombutdiff <darwinorquin16@gmail.com>
2024-07-02 14:54:38 +0800

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ let message = [
   `"You're not just my partner; you're my best friend and confidant."`,
   `"My heart skips a beat every time I think of you."`,
   `"You're the missing piece that completes my puzzle of happiness."`,
+  `"Your love is a treasure I cherish every day."`,
+  `"I am so grateful to have you in my life."`,
+  `"You make me believe in fairy tales and happily ever afters."`,
+  `"With you, I feel like I can conquer the world."`,
+  `"You are my sun, my moon, and all my stars."`,
+  `"Loving you feels like coming home to a place I've never been before."`,
+  `"Your kindness and compassion inspire me to be a better person."`,
+  `"I am the luckiest person alive to have you by my side."`,
+  `"Every day spent with you is a blessing I never want to take for granted."`,
+  `"Thank you for being the reason I smile every day."`,
 ];
 
 let messageText = document.querySelector(".message");
